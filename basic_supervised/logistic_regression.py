1d2bf11bb3d5ea1aff4a8e1fc319ce13ebeaff35
vb690 <vb690@york.ac.uk>
2018-08-26 09:29:21 +0100

    Addedd visualization best fit line, need rework

--- basic_supervised/logistic_regression.py
+++ basic_supervised/logistic_regression.py
@@ -2,6 +2,10 @@ import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.datasets import make_classification
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
 class LogReg():
     def __init__(self, X, y, alpha, epochs=1000):
         self.X = np.mat(X)
@@ -58,3 +62,36 @@ class LogReg():
         plt.xlabel('Weighted X')
         plt.ylabel('Sigmoid (Weighted X)')
         plt.show()
+
+    def visualize_decision_boundary(self, X, y, features):
+        X_0 = X[:, features[0]]
+        X_1 = X[:, features[1]]
+        w_0 = self.listify_matrix(self.weights)[features[0]]
+        w_1 = self.listify_matrix(self.weights)[features[1]]
+
+        x_0 = min(X_0) * w_0
+        x_1 = max(X_0) * w_0
+        y_0 = min(X_1) * w_1
+        y_1 = max(X_1) * w_1
+
+        plt.figure(figsize = (10, 10))
+        plt.scatter(X_0, X_1, c = y)
+        plt.plot([x_0, y_0], [x_1, y_1], color='r', linestyle='-', linewidth=2)
+        plt.xlabel('X 0')
+        plt.ylabel('X 1')
+        plt.show()
+
+
+if __name__ ==  '__main__':
+
+    X, y = make_classification(n_samples = 10000, n_features = 5)
+
+    for train_i, test_i in StratifiedShuffleSplit().split(X, y):
+
+        X_train, y_train = X[train_i], y[train_i]
+        X_test, y_test = X[test_i], y[test_i]
+
+    model = LogReg(X_train, y_train, 0.001)
+    model.fit()
+    model.predict(X_test, y_test)
+    model.visualize_decision_boundary(X_test, y_test, [0, 1])
