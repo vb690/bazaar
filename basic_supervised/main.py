3c3f53ea85fe5e2f60aacc084baddf3c8f3e4283
vb690 <vb690@york.ac.uk>
2018-09-05 23:03:07 +0100

    First working version of Gaussian Naive Bayes

--- basic_supervised/main.py
+++ basic_supervised/main.py
@@ -1,7 +1,8 @@
 from toolbox.tester import *
 from logistic_regression import *
 from k_nearest_neighbour import *
+from naive_bayes import *
 
 if __name__ == '__main__':
 
-    test_classification(Knn(k=3))
+    test_classification(GaussianNaiveBayes())
--- basic_supervised/naive_bayes.py
+++ basic_supervised/naive_bayes.py
@@ -1,9 +1,11 @@
+import math
+
 import numpy as np
 
 class GaussianNaiveBayes():
 
     def __init__(self):
-        pass
+        self.y_predicted = []
 
     @staticmethod
     def group_by(values_array, grouping_array):
@@ -16,7 +18,13 @@ class GaussianNaiveBayes():
         return grouped
 
     @staticmethod
-    def compute_statistics(grouped):
+    def compute_gaussian_pdf(x, mean, std):
+        variance = std**2
+        num = math.exp(-(x - mean)**2 / (2 * variance))
+        denom = math.sqrt(2*math.pi*variance)
+        return num / denom
+
+    def _compute_statistics(self, grouped):
         labels_stats = {}
         for label, values in grouped.items():
 
@@ -28,6 +36,26 @@ class GaussianNaiveBayes():
         labels, occurences = np.unique(self.y_train, return_counts=True)
         return {label : occurence/len(self.y_train) for label, occurence in zip(labels, occurences)}
 
+    def _compute_likelihoods(self, input_X):
+        labels_likelihoods = {}
+        for label, stats in self.labels_stats.items():
+
+            means = stats['means']
+            stds = stats['stds']
+            likelihood = np.prod([self.compute_gaussian_pdf(x, mean, std) for x, mean, std in zip(input_X, means, stds)])
+            labels_likelihoods[label] = likelihood
+
+        return labels_likelihoods
+
+    def _compute_joint_probabilities(self, input_X):
+        labels_joint_probabilities = {}
+        labels_likelihoods = self._compute_likelihoods(input_X)
+        for label, probability in self.labels_probabilities.items():
+
+            labels_joint_probabilities[label] = probability*labels_likelihoods[label]
+
+        return labels_joint_probabilities
+
     def fit(self, X_train, y_train, labels_probabilities = None):
         setattr(self, 'X_train', X_train)
         setattr(self, 'y_train', y_train)
@@ -36,4 +64,18 @@ class GaussianNaiveBayes():
         else:
             setattr(self, 'labels_probabilities', self._compute_probabilities())
         grouped = self.group_by(self.X_train, self.y_train)
-        setattr(self, 'labels_stats', self.compute_statistics(grouped))
+        setattr(self, 'labels_stats', self._compute_statistics(grouped))
+
+    def predict(self, X_test):
+        for input_X in X_test:
+
+            labels_joint_probabilities = self._compute_joint_probabilities(input_X)
+            marginal_probability = sum(labels_joint_probabilities.values())
+            prediction = {}
+            for label, joint_probability in labels_joint_probabilities.items():
+
+                prediction[label] = joint_probability/marginal_probability
+
+            self.y_predicted.append(max(prediction, key=prediction.get))
+
+        return self.y_predicted
