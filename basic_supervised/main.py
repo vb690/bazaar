4605ce77999005ec936bb362c0bb46ba44f01da6
vb690 <vb690@york.ac.uk>
2020-08-03 07:25:12 +0100

    New structure for bazaar

--- basic_supervised/main.py
+++
@@ -1,8 +0,0 @@
-from toolbox.tester import *
-from logistic_regression import *
-from k_nearest_neighbour import *
-from naive_bayes import *
-
-if __name__ == '__main__':
-
-    test_classification(Knn())
