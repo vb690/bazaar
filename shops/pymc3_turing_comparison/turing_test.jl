3b82ffaec3767b3412d5e0460f315b59329d613a
mtaung <myat@aung.uk>
2021-04-26 19:51:33 +0100

    Docs w/ Blue convention. no makedocs() yet

--- shops/pymc3_turing_comparison/turing_test.jl
+++ shops/pymc3_turing_comparison/turing_test.jl
@@ -8,8 +8,22 @@ addprocs(4)
 @everywhere using StatsFuns: logistic
 @everywhere using Turing
 
+"""
+    get_data(path, :y_label)
+
+Returns data for the logistic model split by X, y structure. 
+Retruns included breast cancer data by default.
+
+# Arguments:
+- `path::String="data/breast_cancer.csv"` path to csv file. 
+- `y_label::Symbol=:diagnosis` y label for target/dependent variable
+
+# Returns:
+- `X` Matrix containing X data/exogenous variables.
+- `y` Vector containing y label data/endogenous variables. 
+"""
 @everywhere function get_data(
-        path::String="data//breast_cancer.csv",
+        path::String="data/breast_cancer.csv",
         y_label::Symbol=:diagnosis
     )
     df = DataFrame(CSV.File(path))
@@ -19,6 +33,15 @@ addprocs(4)
     return X, y
 end
 
+"""
+    logistic_regression(x::Matrix{Float64}, y::Vector{Int64})
+
+Creates a logistic model to be used as an input argument in a sampler. 
+
+# Arguments:
+- `X::Matrix{Float64}`: Exogenous input data X in matrix format. 
+- `y::Vector{Int64}`: Endogenous/dependent variables vector.
+"""
 @everywhere @model function logistic_regression(
         X::Matrix{Float64},
         y::Vector{Int64}
@@ -36,6 +59,18 @@ end
     end
 end
 
+"""
+    profiler(X::Matrix{Float64}, y::Vector{Int64}, max_iters::Int64=10)
+
+# Arguments:
+- `X::Matrix{Float64}`: Exogenous variables
+- `y::Matrix{Int64}`: Endogenous variables 
+- `max_iters::Int64`: The specified number of iterations to execute the sampler for logistic regression.
+
+# Returns:
+- `times::Vector{Any}`: Execution times for each iteration.
+- `trace::Chains{Any}`: The trace generated in the final sample. 
+"""
 @everywhere function profiler(
         X::Matrix{Float64},
         y::Vector{Int64},
@@ -46,9 +81,9 @@ end
 
         time = @elapsed global trace = sample(
                 logistic_regression(X, y),
-                NUTS(2, 0.90),
+                NUTS(1000, 0.90),
                 MCMCDistributed(),
-                2,
+                1000,
                 4
             )
         append!(times, time)
@@ -60,7 +95,7 @@ end
     X, y = get_data()
     times, traces = profiler(X, y)
     println(times)
-    return times, traces
+    return times, trace
 end
 
-times, traces = main()
+times, trace = main()
